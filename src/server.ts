9e9b4b67ac4be831583890d59f06d9a371247b91
Mostafiz <mostafiz4372@gmail.com>
2025-12-25 01:57:15 +0600

    solved types errors

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './config';
 
 
 async function bootstrap() {
-    // This variable will hold our server instance
+    // This variable will hold our server instances
     let server: Server;
 
     try {
