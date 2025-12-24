c57e9a6b73289d758a4f2752bffc8e707db7de08
Mostafiz <mostafiz4372@gmail.com>
2025-12-25 00:17:50 +0600

    completed care loop backend

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import config from './config';
+import router from './app/routes';
+import cookieParser from 'cookie-parser'
+import { PaymentController } from './app/modules/payment/payment.controller';
+import cron from 'node-cron';
+import { AppointmentService } from './app/modules/appointment/appointment.service';
+
+const app: Application = express();
+
+app.post(
+    "/webhook",
+    express.raw({ type: "application/json" }),
+    PaymentController.handleStripeWebhookEvent
+);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+
+cron.schedule('* * * * *', () => {
+    try {
+        // console.log("Last Server Running time:", new Date())
+        AppointmentService.cancelUnpaidAppointments();
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.use("/api/v1", router);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        message: "Server is running..",
+        environment: config.node_env,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
+    })
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,71 @@
+import express from 'express';
+import { userRoutes } from '../modules/user/user.routes';
+import { authRoutes } from '../modules/auth/auth.routes';
+import { ScheduleRoutes } from '../modules/schedule/schedule.routes';
+import { doctorScheduleRoutes } from '../modules/doctorSchedule/doctorSchedule.routes';
+import { SpecialtiesRoutes } from '../modules/specialties/specialties.routes';
+import { DoctorRoutes } from '../modules/doctor/doctor.routes';
+import { AdminRoutes } from '../modules/admin/admin.routes';
+import { PatientRoutes } from '../modules/patient/patient.routes';
+import { AppointmentRoutes } from '../modules/appointment/appointment.routes';
+import { PrescriptionRoutes } from '../modules/prescription/prescription.routes';
+import { ReviewRoutes } from '../modules/review/review.routes';
+import { MetaRoutes } from '../modules/meta/meta.routes';
+
+
+const router = express.Router();
+
+const moduleRoutes = [
+    {
+        path: '/user',
+        route: userRoutes
+    },
+    {
+        path: '/auth',
+        route: authRoutes
+    },
+    {
+        path: '/schedule',
+        route: ScheduleRoutes
+    },
+    {
+        path: '/doctor-schedule',
+        route: doctorScheduleRoutes
+    },
+    {
+        path: '/specialties',
+        route: SpecialtiesRoutes
+    },
+    {
+        path: '/doctor',
+        route: DoctorRoutes
+    },
+    {
+        path: '/admin',
+        route: AdminRoutes
+    },
+    {
+        path: '/patient',
+        route: PatientRoutes
+    },
+    {
+        path: '/appointment',
+        route: AppointmentRoutes
+    },
+    {
+        path: '/prescription',
+        route: PrescriptionRoutes
+    },
+    {
+        path: '/review',
+        route: ReviewRoutes
+    },
+    {
+        path: '/metadata',
+        route: MetaRoutes
+    },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route))
+
+export default router;
